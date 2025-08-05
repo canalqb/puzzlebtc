92d890ec17d36af23613e7de1863ca79495bd9dc
CanalQb <qrodrigob@gmail.com>
2025-08-05 11:18:53 -0300

    Create address_range_mapper.py

---
+++ btc-address-analyzer/address_range_mapper.py
@@ -0,0 +1,335 @@
+dados = [ 
+('1073741824','2102388551','2147483647'),
+('2147483648','3093472814','4294967295'),
+('4294967296','7137437912','8589934591'),
+('8589934592','14133072157','17179869183'),
+('17179869184','20112871792','34359738367'),
+('34359738368','42387769980','68719476735'),
+('68719476736','100251560595','137438953471'),
+('137438953472','146971536592','274877906943'),
+('274877906944','323724968937','549755813887'),
+('549755813888','1003651412950','1099511627775'),
+('1099511627776','1458252205147','2199023255551'),
+('2199023255552','2895374552463','4398046511103'),
+('4398046511104','7409811047825','8796093022207'),
+('8796093022208','15404761757071','17592186044415'),
+('17592186044416','19996463086597','35184372088831'),
+('35184372088832','51408670348612','70368744177663'),
+('70368744177664','119666659114170','140737488355327'),
+('140737488355328','191206974700443','281474976710655'),
+('281474976710656','409118905032525','562949953421311'),
+('562949953421312','611140496167764','1125899906842623'),
+('1125899906842624','2058769515153876','2251799813685247'),
+('2251799813685248','4216495639600700','4503599627370495'),
+('4503599627370496','6763683971478124','9007199254740991'),
+('9007199254740992','9974455244496708','18014398509481984'),
+('18014398509481984','30045390491869460','36028797018963968'),
+('36028797018963968','44218742292676576','72057594037927936'),
+('72057594037927936','138245758910846496','144115188075855872'),
+('144115188075855872','199976667976342048','288230376151711744'),
+('288230376151711744','525070384258266176','576460752303423488'),
+('576460752303423488','1135041350219496320','1152921504606846976'),
+('1152921504606846976','1425787542618654976','2305843009213693952'),
+('2305843009213693952','3908372542507822080','4611686018427387904'),
+('4611686018427387904','8993229949524469760','9223372036854775808'),
+('9223372036854775808','17799667357578235904','18446744073709551616'),
+('18446744073709551616','30568377312064204800','36893488147419103232'),
+('36893488147419103232','46346217550346338304','73786976294838206464'),
+('73786976294838206464','132656943602386255872','147573952589676412928'),
+('147573952589676412928','219898266213316034560','295147905179352825856'),
+('295147905179352825856','297274491920375873536','590295810358705651712'),
+('590295810358705651712','970436974005023670272','1180591620717411303424'), 
+('18889465931478580854784','22538323240989824647168','37778931862957161709568'), 
+('604462909807314587353088','1105520030589234431655936','1208925819614629174706176'), 
+('19342813113834066795298816','21090315766411507338838016','38685626227668133590597632'), 
+('618970019642690137449562112','868012190417726377904046080','1237940039285380274899124224'), 
+('19807040628566084398385987584','25525831956644114284329041920','39614081257132168796771975168'), 
+('633825300114114700748351602688','868221233689326482094685159424','1267650600228229401496703205376'), 
+('20282409603651670423947251286016','29083230144918043474352194715648','40564819207303340847894502572032'), 
+('649037107316853453566312041152512','1090246098153987122742007059972096','1298074214633706907132624082305024'),  
+('20769187434139310514121985316880384','31464123230573853396965426819235840','41538374868278621028243970633760768'), 
+('664613997892457936451903530140172288','919343500840980287176338231525376000','1329227995784915872903807060280344576'), 
+('21267647932558653966460912964485513216','37650549717742543066237230173355245568','42535295865117307932921825928971026432'), 
+('680564733841876926926749214863536422912','1103873984953507513725582045078716678144','1361129467683753853853498429727072845824'),  
+
+]
+
+data_address = [
+"122AJhKLEfkFBaGAd84pLp1kfE7xK3GdT8",
+"128z5d7nN7PkCuX5qoA4Ys6pmxUYnEy86k",
+"12CiUhYVTTH33w3SPUBqcpMoqnApAV4WCF",
+"12jbtzBb54r97TCwW3G1gCFoumpckRAPdY",
+"12JzYkkN76xkwvcPT6AWKZtGX6w2LAgsJg",
+"12VVRNPi4SJqUTsp6FmqDqY5sGosDtysn4",
+"13A3JrvXmvg5w9XGvyyR4JEJqiLz8ZySY3",
+"13N66gCzWWHEZBxhVxG18P8wyjEWF9Yoi1",
+"13Q84TNNvgcL3HJiqQPvyBb9m4hxjS3jkV",
+"13zb1hQbWVsc2S7ZTZnP2G4undNNpdh5so",
+"13zYrYhhJxp6Ui1VV7pqa5WDhNWM45ARAC",
+"14iXhn8bGajVWegZHJ18vJLHhntcpL4dex",
+"14JHoRAdmJg3XR4RjMDh6Wed6ft6hzbQe9",
+"14MdEb4eFcT3MVG5sPFG4jGLuHJSnt1Dk2",
+"14oFNXucftsHiUMY8uctg6N487riuyXs4h",
+"14u4nA5sugaswb6SZgn5av2vuChdMnD9E5",
+"15ANYzzCp5BFHcCnVFzXqyibpzgPLWaD8b",
+"15c9mPGLku1HuW9LRtBf4jcHVpBUt8txKz",
+"15EJFC5ZTs9nhsdvSUeBXjLAuYq3SWaxTc",
+"15JhYXn6Mx3oF4Y7PcTAv2wVVAuCFFQNiP",
+"15K1YKJMiJ4fpesTVUcByoz334rHmknxmT",
+"15MnK2jXPqTMURX4xC3h4mAZxyCcaWWEDD",
+"15nf31J46iLuK1ZkTnqHo7WgN5cARFK3RA",
+"15qF6X51huDjqTmF9BJgxXdt1xcj46Jmhb",
+"15qsCm78whspNQFydGJQk5rexzxTQopnHZ",
+"15z9c9sVpu6fwNiK7dMAFgMYSK4GqsGZim",
+"16AbnZjZZipwHMkYKBSfswGWKDmXHjEpSf",
+"16jY7qLJnxb7CHZyqBP8qca9d51gAjyXQN",
+"16RGFo6hjq9ym6Pj7N5H7L1NR1rVPJyw2v",
+"16zRPnT8znwq42q7XeMkZUhb1bKqgRogyy",
+"174SNxfqpdMGYy5YQcfLbSTK3MRNZEePoy",
+"17aPYR1m6pVAacXg1PTDDU7XafvK1dxvhi",
+"17Q7tuG2JwFFU9rXVj3uZqRtioH3mx2Jad",
+"17s2b9ksz5y7abUm92cHwG8jEPCzK3dLnT",
+"17uDfp5r4n441xkgLFmhNoSW1KWp6xVLD",
+"18192XpzzdDi2K11QVHR7td2HcPS6Qs5vg",
+"1824ZJQ7nKJ9QFTRBqn7z7dHV5EGpzUpH3",
+"187swFMjz1G54ycVU56B7jZFHFTNVQFDiu",
+"18A7NA9FTsnJxWgkoFfPAFbQzuQxpRtCos",
+"18aNhurEAJsw6BAgtANpexk5ob1aGTwSeL",
+"18KsfuHuzQaBTNLASyj15hy4LuqPUo1FNB",
+"18ywPwj39nGjqBrQJSzZVq2izR12MDpDr8",
+"18ZMbwUFLMHoZBbfpCjUJQTCMCbktshgpe",
+"19EEC52krRUK1RkUAEZmQdjTyHT7Gp1TYT",
+"19eVSDuizydXxhohGh8Ki9WY9KsHdSwoQC",
+"19GpszRNUej5yYqxXoLnbZWKew3KdVLkXg",
+"19QciEHbGVNY4hrhfKXmcBBCrJSBZ6TaVt",
+"19vkiEajfhuZ8bs8Zu2jgmC6oqZbWqhxhG",
+"19YZECXj3SxEZMoUeJ1yiPsw8xANe7M7QR",
+"19z6waranEf8CcP8FqNgdwUe1QRxvUNKBG",
+"19ZewH8Kk1PDbSNdJ97FP4EiCjTRaZMZQA",
+"1Ab4vzG6wEQBDNQM1B2bvUz4fqXXdFk2WT",
+"1AcAmB6jmtU6AiEcXkmiNE9TNVPsj9DULf",
+"1AE8NzzgKE7Yhz7BWtAcAAxiFMbPo82NB5",
+"1AoeP37TmHdFh8uN72fu9AqgtLrUwcv2wJ",
+"1ARk8HWJMn8js8tQmGUJeQHjSE7KRkn2t8",
+"1AVJKwzs9AskraJLGHAZPiaZcrpDr1U6AB",
+"1AWCLZAjKbV1P7AHvaPNCKiB7ZWVDMxFiz",
+"1BCf6rHUW6m3iH2ptsvnjgLruAiPQQepLe",
+"1BDyrQ6WoF8VN3g9SAS1iKZcPzFfnDVieY",
+"1Be2UF9NLfyLFbtm3TCbmuocc9N1Kduci1",
+"1BgGZ9tcN4rm9KBzDn7KprQz87SZ26SAMH",
+"1BkkGsX9ZM6iwL3zbqs7HWBV7SvosR6m8N",
+"1Bxk4CQdqL9p22JEtDfdXMsng1XacifUtE",
+"1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9",
+"1CaBVPrwUxbQYYswu32w7Mj4HR4maNoJSX",
+"1CD91Vm97mLQvXhrnoMChhJx4TP9MaQkJo",
+"1CdufMQL892A69KXgv6UNBD17ywWqYpKut",
+"1CfZWK1QTQE3eS9qn61dQjV89KDjZzfNcv",
+"1CKCVdbDJasYmhswB6HKZHEAnNaDpK7W4n",
+"1CkR2uS7LmFwc3T2jV8C1BhWb5mQaoxedF",
+"1CMjscKB3QW7SDyQ4c3C3DEUHiHRhiZVib",
+"1CMq3SvFcVEcpLMuuH8PUcNiqsK1oicG2D",
+"1CQFwcjw1dwhtkVWBttNLDtqL7ivBonGPV",
+"1CUNEBjYrCn2y1SdiUMohaKUi4wpP326Lb",
+"1CXvTzR6qv8wJ7eprzUKeWxyGcHwDYP1i2",
+"1DBaumZxUkM4qMQRt2LVWyFJq5kDtSZQot",
+"1DFYhaB2J9q1LLZJWKTnscPWos9VBqDHzv",
+"1DJh2eHFYQfACPmrvpyWc8MSTYKh7w9eRF",
+"1Dn8NF8qDyyfHMktmuoQLGyjWmZXgvosXf",
+"1E32GPWgDyeyQac4aJxm9HVoLrrEYPnM4N",
+"1E6NuFjCi27W5zoXg8TRdcSRq84zJeBW3k",
+"1EeAxcprB2PpCnr34VfZdFrkUWuxyiNEFv",
+"1EhqbyUMvvs7BfL8goY6qcPbD6YKfPqb7e",
+"1EQJvpsmhazYCcKX5Au6AZmZKRnzarMVZu",
+"1ErZWg5cFCe4Vw5BzgfzB74VNLaXEiEkhk",
+"1EzVHtmbN4fs4MiNk3ppEnKKhsmXYJ4s74",
+"1F3JRMWudBaj48EhwcHDdpeuy2jwACNxjP",
+"1Fo65aKq8s8iquMt6weF1rku1moWVEd5Ua",
+"1FRoHA9xewq7DjrZ1psWJVeTer8gHRqEvR",
+"1FTpAbQa4h8trvhQXjXnmNhqdiGBd1oraE",
+"1FWGcVDK3JGzCC3WtkYetULPszMaK2Jksv",
+"1FwZXt6EpRT7Fkndzv6K4b4DFoT4trbMrV",
+"1Fz63c775VV9fNyj25d9Xfw3YHE6sKCxbt",
+"1G6EFyBRU86sThN3SSt3GrHu1sA7w7nzi4",
+"1GDSuiThEV64c166LUFC9uDcVdGjqkxKyh",
+"1GnNTmTVLZiqQfLbAdp9DVdicEnB5GoERE",
+"1GuBBhf61rnvRe4K8zu8vdQB3kHzwFqSy7",
+"1GvgAXVCbA8FBjXfWiAms4ytFeJcKsoyhL",
+"1HAX2n9Uruu9YDt4cqRgYcvtGvZj1rbUyt",
+"1HB1iKUqeffnVsvQsbpC6dNi1XKbyNuqao",
+"1HBtApAFA9B2YZw3G2YKSMCtb3dVnjuNe2",
+"1HduPEXZRdG26SUT5Yk83mLkPyjnZuJ7Bm",
+"1HsMJxNiV7TLxmoF6uJNkydxPFDog4NQum",
+"1Hz3uv3nNZzBVMXLGadCucgjiCs5W9vaGz",
+"1J36UjUByGroXcCvmj13U6uwaVv9caEeAt",
+"1JTK7s9YVYywfm5XUH7RNhHJH1LshCaRFR",
+"1JVnST957hGztonaWK6FougdtjxzHzRMMg",
+"1JWnE6p6UN7ZJBN7TtcbNDoRcjFtuDWoNL",
+"1K3x5L6G57Y494fDqBfrojD28UJv4s5JcK",
+"1K6xGMUbs6ZTXBnhw1pippqwK6wjBWtNpL",
+"1KCgMv8fo2TPBpddVi9jqmMmcne9uSNJ5F",
+"1Kh22PvXERd2xpTQk3ur6pPEqFeckCJfAr",
+"1Kn5h2qpgw9mWE5jKpk8PP4qvvJ1QVy8su",
+"1KNRfGWw7Q9Rmwsc6NT5zsdvEb9M2Wkj5Z",
+"1KrU4dHE5WrW8rhWDsTRjR21r8t3dsrS3R",
+"1KYUv7nSvXx4642TKeuC2SNdTk326uUpFy",
+"1L12FHH2FHjvTviyanuiFVfmzCy46RRATU",
+"1L2GM8eE7mJWLdo3HZS6su1832NX2txaac",
+"1L5sU9qvJeuwQUdt4y1eiLmquFxKjtHr3E",
+"1LeBZP5QCwwgXRtmVUvTVrraqPUokyLHqe",
+"1LhE6sCTuGae42Axu1L1ZB7L96yi9irEBE",
+"1LHtnpd8nU5VHEMkG2TMYYNUjjLc992bps",
+"1LuUHyrQr8PKSvbcY1v1PiuGuqFjWpDumN",
+"1LzhS3k3e9Ub8i2W1V8xQFdB8n2MYCHPCa",
+"1M7ipcdYHey2Y5RZM34MBbpugghmjaV89P",
+"1M92tSqNmQLYw33fuBvjmeadirh1ysMBxK",
+"1McVt1vMtCC7yn5b9wgX1833yCcLXzueeC",
+"1Me3ASYt5JCTAK2XaC32RMeH34PdprrfDx",
+"1Me6EfpwZK5kQziBwBfvLiHjaPGxCKLoJi",
+"1MEzite4ReNuWaL5Ds17ePKt2dCxWEofwk",
+"1MnJ6hdhvK37VLmqcdEwqC3iFxyWH2PHUV",
+"1MUJSJYtGPVGkBCTqGspnxyHahpt5Te8jy",
+"1MVDYgVaSN6iKKEsbzRUAYFrYJadLYZvvZ",
+"1MZ2L1gFrCtkkn6DnTT2e4PFUTHw9gNwaj",
+"1NBC8uXJy1GiJ6drkiZa1WuKn51ps7EPTv",
+"1NeGn21dUDDeqFQ63xb2SpgUuXuBLA4WT4",
+"1NevxKDYuDcCh1ZMMi6ftmWwGrZKC6j7Ux",
+"1NgVmsCCJaKLzGyKLFJfVequnFW9ZvnMLN",
+"1NLbHuJebVwUZ1XqDjsAyfTRUPwDQbemfv",
+"1NpnQyZ7x24ud82b7WiRNvPm6N8bqGQnaS",
+"1NpYjtLira16LfGbGwZJ5JbDPh3ai9bjf4",
+"1NtiLNGegHWE3Mp9g2JPkgx6wUg4TW7bbk",
+"1NWmZRpHH4XSPwsW6dsS3nrNWfL1yrJj4w",
+"1Pd8VvT49sHKsmqrQiP61RsVwmXCZ6ay7Z",
+"1PgQVLmst3Z314JrQn5TNiys8Hc38TcXJu",
+"1Pie8JkxBT6MGPz9Nvi3fsPkr2D8q3GBc1",
+"1PiFuqGpG8yGM5v6rNHWS3TjsG6awgEGA1",
+"1PitScNLyp2HCygzadCh7FveTnfmpPbfp8",
+"1PJZPzvGX19a7twf5HyD2VvNiPdHLzm9F6",
+"1PWABE7oUahG2AFFQhhvViQovnCr4rEv7Q",
+"1PWCx5fovoEaoBowAvF5k91m2Xat9bMgwb",
+"1PWo3JeB9jrGwfHDNpdGK54CRas7fsVzXU",
+"1PXAyUB8ZoH3WD8n5zoAthYjN15yN5CVq5",
+"1PxH3K1Shdjb7gSEoTX7UPDZ6SH4qGPrvq",
+"1PXv28YxmYMaB8zxrKeZBW8dt2HK7RkRPX",
+"1QCbW9HWnwQWiQqVo5exhAnmfqKRrCRsvW",
+"1QKBaU6WAeycb3DbKbLBkX7vJiaS8r42Xo",
+"1rSnXMr63jdCuegJFuidJqWxUPV7AtUf7",
+"1UDHPdovvR985NrWSkdWQDEQ1xuRiTALq",
+
+]
+
+import os
+import csv
+from decimal import Decimal, getcontext, ROUND_FLOOR
+import math
+from bit import Key
+
+# Configuração de precisão
+decimal_places = 6
+getcontext().prec = 100  # Alta precisão
+linhas = []
+
+def formatar_decimal(d):
+    s = f"{d:.{decimal_places}f}".replace('.', ',')
+    return s
+
+for inicio_str, meio_str, fim_str in dados:
+    inicio = Decimal(inicio_str)
+    fim = Decimal(fim_str)
+    intervalo = fim - inicio
+
+    if meio_str is not None and meio_str.lower() != 'none':
+        meio = Decimal(meio_str)
+
+        if intervalo == 0:
+            passos_ate_meio = Decimal(0)
+            passos_meio_ate_fim = Decimal(0)
+            meio_calculado_1 = inicio
+            meio_calculado_2 = fim
+        else:
+            proporcao_meio = (meio - inicio) / intervalo
+            proporcao_fim = (fim - meio) / intervalo
+
+            passos_ate_meio = proporcao_meio * Decimal(255)
+            passos_meio_ate_fim = proporcao_fim * Decimal(255)
+
+            meio_calculado_1 = inicio + (passos_ate_meio / Decimal(255)) * intervalo
+            meio_calculado_2 = fim - (passos_meio_ate_fim / Decimal(255)) * intervalo
+
+        diff_1 = meio_calculado_1 - meio
+        diff_2 = meio_calculado_2 - meio
+        diff_3 = meio_calculado_2 - meio_calculado_1
+
+        linhas.append([
+            f"'{inicio}",
+            f"'{meio}",
+            f"'{fim}",
+            f"'{formatar_decimal(passos_ate_meio)}",
+            f"'{formatar_decimal(passos_meio_ate_fim)}",
+            f"'{formatar_decimal(meio_calculado_1)}",
+            f"'{formatar_decimal(meio_calculado_2)}",
+            f"'{formatar_decimal(diff_1)}",
+            f"'{formatar_decimal(diff_2)}",
+            f"'{formatar_decimal(diff_3)}"
+        ])
+    else:
+        print(f"\n[Interpolando de {inicio} até {fim} (meio ausente)]")
+        loop_255 = Decimal('0.00')
+        passo = Decimal('0.001')
+        inteiros_vistos = set()
+        
+        total_loops = Decimal('510.00')  # você pode mudar isso depois, como Decimal('512.00'), etc.
+        while loop_255 <= total_loops: 
+            meio_calculado = inicio + (loop_255 / Decimal(255)) * intervalo
+            inteiro = int(meio_calculado.quantize(Decimal('1'), rounding=ROUND_FLOOR))
+
+            if inteiro not in inteiros_vistos:
+                hex_key = f"{inteiro:064x}"
+                key = Key.from_hex(hex_key)
+                wif = key.to_wif()
+                address = key.address 
+
+                if address in data_address:
+                    os.system('cls' if os.name == 'nt' else 'clear')
+                    print(f"✅ Encontrado! WIF: {wif} - BTC: {address}")
+                    with open('enderecos_encontrados.txt', 'a', encoding='utf-8') as f:
+                        f.write(f"WIF: {wif} - BTC: {address}\n")
+                    break
+                else:
+                    print(f"log2(inicio) ≈ {math.log2(inicio):.2f} - loop_255 ≈ {loop_255:.2f}, INTEIRO: {inteiro} - WIF {wif} - BTC {address}", end="\r")
+
+                inteiros_vistos.add(inteiro)
+
+            loop_255 += passo
+
+        linhas.append([
+            f"'{inicio}",
+            f"'None",
+            f"'{fim}",
+            "'",
+            "'",
+            "'",
+            "'",
+            "'",
+            "'",
+            "'"
+        ])
+        print()
+        
+
+# Exportar CSV
+with open("resultado_proporcional_decimal_virgula.csv", "w", newline="", encoding="utf-8") as csvfile:
+    writer = csv.writer(csvfile, delimiter=';')
+    writer.writerow([
+        "Inicio", "Meio", "Fim",
+        "Passos_proporcionais_ate_meio",
+        "Passos_proporcionais_meio_ate_fim",
+        "Meio_calculado_1",
+        "Meio_calculado_2",
+        "Diff_meio_calculado_1_medio",
+        "Diff_meio_calculado_2_medio",
+        "Diff_meio_calculado_2_meio_calculado_1"
+    ])
+    writer.writerows(linhas)
+
+print("\n📁 Arquivo 'resultado_proporcional_decimal_virgula.csv' criado com sucesso.")
